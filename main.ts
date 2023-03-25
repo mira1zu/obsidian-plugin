df18abdb357b067c29e92d798111634827dabaec
Bohdan Poshtarenko <mira1zu@mira1zu.com>
2023-03-25 17:00:34 +0100

    Added basic functionality

--- main.ts
+++ main.ts
@@ -1,7 +1,30 @@
-import { Plugin } from 'obsidian';
+import { Plugin, TFile, TFolder, Vault } from 'obsidian';
 
 export default class MyPlugin extends Plugin {
 	onload() {
+		const vault = this.app.vault;
+		const workspace = this.app.workspace;
 
+		workspace.onLayoutReady(() => {
+			const fileTree = vault.getRoot();
+
+			const files: TFile[] = [];
+
+			// TODO: add support for selection desired folder(s)
+			Vault.recurseChildren(fileTree, (file) => {
+				if (file instanceof TFolder) {
+					return;
+				}
+
+				files.push(<TFile>file);
+			})
+
+			const filesAmount = files.length;
+
+			const randomNumber = Math.floor(Math.random() * (filesAmount + 1));
+			const randomFile = files[randomNumber];
+
+			workspace.getLeaf(true).openFile(randomFile);
+		});
 	}
 }
