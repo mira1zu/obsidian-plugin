9dd25b9ab7a4882029cb444a2bc12a5bcb9b0dec
Bohdan Poshtarenko <mira1zu@mira1zu.com>
2023-03-25 18:56:34 +0100

    Tabs -> spaces (#4)

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,30 @@
 import { Plugin, TFile, TFolder, Vault } from 'obsidian';
 
 export default class MyPlugin extends Plugin {
-	onload() {
-		const vault = this.app.vault;
-		const workspace = this.app.workspace;
+    onload() {
+        const vault = this.app.vault;
+        const workspace = this.app.workspace;
 
-		workspace.onLayoutReady(() => {
-			const fileTree = vault.getRoot();
+        workspace.onLayoutReady(() => {
+            const fileTree = vault.getRoot();
 
-			const files: TFile[] = [];
+            const files: TFile[] = [];
 
-			// TODO: add support for selection desired folder(s)
-			Vault.recurseChildren(fileTree, (file) => {
-				if (file instanceof TFolder) {
-					return;
-				}
+            // TODO: add support for selection desired folder(s)
+            Vault.recurseChildren(fileTree, (file) => {
+                if (file instanceof TFolder) {
+                    return;
+                }
 
-				files.push(<TFile>file);
-			})
+                files.push(<TFile>file);
+            })
 
-			const filesAmount = files.length;
+            const filesAmount = files.length;
 
-			const randomNumber = Math.floor(Math.random() * (filesAmount + 1));
-			const randomFile = files[randomNumber];
+            const randomNumber = Math.floor(Math.random() * (filesAmount + 1));
+            const randomFile = files[randomNumber];
 
-			workspace.getLeaf(true).openFile(randomFile);
-		});
-	}
+            workspace.getLeaf(true).openFile(randomFile);
+        });
+    }
 }
